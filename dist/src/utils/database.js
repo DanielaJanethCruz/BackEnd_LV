d6e16d18ca5beee927817a71a48336e209044366
DanielaJanethCruz <daniela122cruz.11@gmail.com>
2024-08-02 11:41:40 -0600

    Carpeta Dist

---
+++ dist/src/utils/database.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    keys: {
+        secret: ')(/&%$webintegral$#&/%'
+    },
+    database: {
+        host: '127.0.0.1',
+        port: 3306,
+        user: 'root',
+        password: 'root',
+        database: 'web_integral'
+    }
+};
+//# sourceMappingURL=database.js.map
